2f167b8e059ef48cb824951f33ac4e8e4a4494a9
sosen7874 <sosen7874@users.noreply.github.com>
2017-11-18 15:52:45 +0900

    rr
    rr

---
+++ src/test1/Testsum.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Testsum {
+ 
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int c = a+b;
+	
+         System.out.println("a와 b의 합은 : " + c);
+ }
+}
